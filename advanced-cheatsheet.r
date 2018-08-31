ab2f095ab876e091b07f59f6475b6eea699c1280
parjoona <parjoona@hotmail.com>
2018-08-31 16:12:15 +0200

    Added library functions

--- advanced-cheatsheet.r
+++ advanced-cheatsheet.r
@@ -49,3 +49,29 @@ Sys.Date() # TODAY
 date <- "1990-01-01" %>% as.Date()
 # Or formating like this
 my.date <- as.Date("Nov-03-90", format = "%b-%d-%y")
+# Most used, includes time
+as.POSTXCT()
+# Should be used
+strptime()
+
+# Pipe operator
+# Works like promises 
+result <- mtcars %>% filter(mpq > 20) %>% sample_n(size = 5) %>% arrange(desc(mpq))
+
+# Data manipulation
+library(Dplyr) # Manipulation of data
+fliter(flights, month == 11, day == 3, carrier, 'AA') # Overrules the normal filter
+slice(flights, 1:10) # Returns the amount of rows in a dataframe 
+arrange(flights, year, month, day, airtime) # Reorders the row after parameters
+select(flights, carrier) # Takes out parameters with lesser syntax
+rename(flights, airline_carrier = carrier) # Renames columns, replaces the old
+distinct(select(flights, carrier)) # Takes all the unique values
+mutate(flights, new_col = arr_delay-dep_deplay) # Creates a new column and remakes the args
+transmute() # Same as mutate, but returns the "new row"
+summarise(flights, avg_air_time = mean(air_time, na.rm = T)) # Works like aggregate
+sample_n(flights, 10) # Returns random rows (10)
+sample_frac(flights, 0.1) # Returns a fraction of rows (%)
+
+library(tidyr) # Cleaning Data
+gather(df, Quarter, Revenue, Quarter1:Quarter4) # Collapses multiple columns into fewer
+spread(stocks, price) # Reverses the gather
