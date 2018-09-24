fcd07f550305f5fb9fc10223948a727a790d314b
parjoona <parjoona@hotmail.com>
2018-09-24 13:16:37 +0200

    added machine learnin introduction

---
+++ machine-learning/introduction.r
@@ -0,0 +1,28 @@
+'https://www-bcf.usc.edu/~gareth/ISL/'
+# Free machine learning book
+
+Machine learning is a method of data analysis
+# Made to automate analytical model building
+# Uses algorithms to learn from data
+# Finds hidden insights without being told to.
+
+Process of machine learning
+# Data acquision
+# Cleaning data, model training & building
+# Cleaning data, test data
+# Model training repeat
+# Model deployment
+
+Supervised learning
+# Algorithms are trained using labeled examples
+# Input is where the output is know
+# Recives inputs along with correct outputs
+# Compares these to find errors
+# > Modifies the model accordingly
+
+Unsupervised learning
+# Takes no historical labels
+# Does not know the right answer, figures it out
+# Explores data to find structure
+# Uses for example, learnest neightbor mapping
+# k-means clustering or single value decomposition
